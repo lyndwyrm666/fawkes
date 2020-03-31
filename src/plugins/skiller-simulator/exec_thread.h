b6c37fcc15a747d85277eb26ca7f2ae816851afb
Tarik Viehmann <viehmann@kbsg.rwth-aachen.de>
2020-03-31 11:16:38 +0200

    skiller-simulator: only compute execution time once per call
    This is necessary in order to ensure that non-deterministic skill
    duration computations are handled correctly.

--- src/plugins/skiller-simulator/exec_thread.h
+++ src/plugins/skiller-simulator/exec_thread.h
@@ -60,6 +60,7 @@ private:
 	void                      execute_skill(const std::string &skill);
 	fawkes::SkillerInterface *skiller_if_;
 	float                     default_skill_runtime_;
+	float                     current_skill_runtime_;
 	fawkes::Time              skill_starttime_;
 	fawkes::skiller_simulator::ExecutionTimeEstimatorManager       execution_time_estimator_manager_;
 	fawkes::skiller_simulator::ExecutionTimeEstimatorsAspectIniFin provider_inifin_;
